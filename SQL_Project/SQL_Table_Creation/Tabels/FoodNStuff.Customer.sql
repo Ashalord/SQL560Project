39494fa79a42d68cf1d9c6cc652c119cbfb0a3ed
jacob <sticca@ksu.edu>
2019-11-18 12:22:36 -0600

    Fixed Adding everything

--- SQL_Project/SQL_Table_Creation/Tabels/FoodNStuff.Customer.sql
+++ SQL_Project/SQL_Table_Creation/Tabels/FoodNStuff.Customer.sql
@@ -3,6 +3,6 @@ BEGIN
    Create table FoodNStuff.Customer(
 	CustomerID int not null identity(1,1) primary key,
 	CustomerName nvarchar(32) not null,
-	CardNumber int not null unique
+	CardNumber nvarchar(128) not null unique
 );
 END
