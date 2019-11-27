4ee7d18eaf1974f2351b005f6aeb72b54c340ad6
jacob <sticca@ksu.edu>
2019-11-26 22:01:30 -0600

    UI displays correct information from tabel

--- SQL_Project/Form1.Designer.cs
+++ SQL_Project/Form1.Designer.cs
@@ -29,26 +29,38 @@
         private void InitializeComponent()
         {
             this.TEST_BUTTON = new System.Windows.Forms.Button();
+            this.dataGridView = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView)).BeginInit();
             this.SuspendLayout();
             // 
             // TEST_BUTTON
             // 
-            this.TEST_BUTTON.Location = new System.Drawing.Point(550, 239);
+            this.TEST_BUTTON.Location = new System.Drawing.Point(409, 12);
             this.TEST_BUTTON.Name = "TEST_BUTTON";
-            this.TEST_BUTTON.Size = new System.Drawing.Size(224, 199);
+            this.TEST_BUTTON.Size = new System.Drawing.Size(113, 88);
             this.TEST_BUTTON.TabIndex = 0;
             this.TEST_BUTTON.Text = "TEST";
             this.TEST_BUTTON.UseVisualStyleBackColor = true;
             this.TEST_BUTTON.Click += new System.EventHandler(this.TEST_BUTTON_Click);
+            // 
+            // dataGridView
+            // 
+            this.dataGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView.Location = new System.Drawing.Point(12, 12);
+            this.dataGridView.Name = "dataGridView";
+            this.dataGridView.Size = new System.Drawing.Size(381, 426);
+            this.dataGridView.TabIndex = 1;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.dataGridView);
             this.Controls.Add(this.TEST_BUTTON);
             this.Name = "Form1";
             this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -56,6 +68,7 @@
         #endregion
 
         private System.Windows.Forms.Button TEST_BUTTON;
+        private System.Windows.Forms.DataGridView dataGridView;
     }
 }
 
--- SQL_Project/Form1.cs
+++ SQL_Project/Form1.cs
@@ -9,11 +9,16 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using SQL_Project.SQL_Table_Creation.DATA;
+using System.Data.SqlClient;
 
 namespace SQL_Project
 {
     public partial class Form1 : Form
     {
+        SqlConnection con = new SqlConnection(@"Server=(localdb)\MSSQLLocalDB;Database=master;Trusted_Connection=True;");
+        SqlDataAdapter da;
+        DataSet ds;
+
         public Form1()
         {
             CreateTablesScript cts = new CreateTablesScript();
@@ -25,8 +30,19 @@ namespace SQL_Project
 
         private void TEST_BUTTON_Click(object sender, EventArgs e)
         {
-            SqlCommandExecutor SQL = new SqlCommandExecutor(@"Server=(localdb)\MSSQLLocalDB;Database=master;Trusted_Connection=True;");
-            SQL.RunSqlCommand(Path.Combine(@"C:\Users\jacob\source\repos\SQL560Project\SQL_Project\SQL_Table_Creation\Tabels", "MakeSchema.sql"));
+            string query = "Select * From FoodNStuff.JobType";
+            da = new SqlDataAdapter(query, con);
+            ds = new DataSet();
+            da.Fill(ds);
+            con.Close();
+
+            if(ds.Tables[0].Rows.Count != 0)
+            {
+                dataGridView.DataSource = ds.Tables[0];
+            }
+
+            //SqlCommandExecutor SQL = new SqlCommandExecutor(@"Server=(localdb)\MSSQLLocalDB;Database=master;Trusted_Connection=True;");
+            //SQL.RunSqlCommand(Path.Combine(@"C:\Users\jacob\source\repos\SQL560Project\SQL_Project\SQL_Table_Creation\Tabels", "MakeSchema.sql"));
         }
     }
 }
